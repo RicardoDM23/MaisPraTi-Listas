f022809f5dd8c507421065db7e8e1b458cc15487
RicardoDM23 <ricardo.dm23@gmail.com>
2024-05-20 19:23:39 -0300

    41

---
+++ CodificaLista02/Lista02_Exercicio41.js
@@ -0,0 +1,20 @@
+/*
+    41. Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de
+    idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui
+    nome e idade.
+*/
+
+// Ricardo D. Muenchen 20/05/2024
+
+let pessoa = {
+    nome: 'Ricardo',
+    idade: 32
+}
+
+console.log(`Idade: ${pessoa.idade}`)
+
+console.log('Obejeto original:' )
+console.log(pessoa)
+pessoa.email = 'pessoa@email.com'
+console.log('Adicionado email:' )
+console.log(pessoa)
