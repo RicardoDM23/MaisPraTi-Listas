93248d83e30be0e3ae37120fa6161fcfeaf86234
RicardoDM23 <ricardo.dm23@gmail.com>
2024-08-29 15:33:47 -0300

    Atividade 5 ajustes

--- CodificaAtividade05/multi-app/src/components/Login.jsx
+++ CodificaAtividade05/multi-app/src/components/Login.jsx
@@ -54,7 +54,7 @@ const Login = ({ onLogin }) => {
   // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
     e.preventDefault(); // Previne o comportamento padrão do formulário
-    if (username === 'admin' && password === '1234') {
+    if (username === 'admin' && password === 'admin') {
       onLogin(); // Chama a função onLogin passada como prop se as credenciais estiverem corretas
     } else {
       alert('Invalid credentials'); // Exibe um alerta se as credenciais estiverem incorretas
